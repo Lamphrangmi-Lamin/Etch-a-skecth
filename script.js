c9e3aee6529b0c4c08b46e261fae49cc1fbe805c
Lamphrangmi-Lamin <lamphrangmilamin46169@gmail.com>
2022-09-19 16:22:51 +0530

    Added an eraser function; modifying the function related to the color pallete; added a function to generate random RGB color; added eventhandlers for colorPicker & RGB button;

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+// Reference to important variables and constants
 const gridContainer = document.getElementById('container');
+const cells = document.querySelectorAll('.cell');
+const colorPicker = document.querySelector('#colorpicker');
+const reset = document.querySelector('#reset');
+const rgb = document.querySelector('#rgb');
+const eraser = document.querySelector('#eraser');
+
+
 function makeGrid() {
     for (let i = 0; i < 16; i++) {
         const row = document.createElement('div');
@@ -11,22 +19,52 @@ function makeGrid() {
         gridContainer.appendChild(row);
     }
 }
+
+// generating random RGB color
+function getRandomColor(){
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let color = `rgb(${r}, ${g}, ${b})`;
+    return color;
+}
+
+// Color pallete
+function chosenColor() {
+    let colorPicked = colorpicker.value;
+    let cell = gridContainer.children;
+    for (let i = 0; i < cell.length; i++) {
+        cell[i].addEventListener('mouseover', function(event) {
+            event.target.style.background = colorPicked;
+        })
+    }
+}
+
+// setting the backgroundColor of the cells to none
+function eraseBtn() {
+    let cell = gridContainer.children;
+    for (let i = 0; i < cell.length; i++) {
+        cell[i].addEventListener('mouseover', function(e) {
+            e.target.style.background = '';
+        })
+    }
+}
+
 makeGrid();
 
-// function eraseBtn() {
-    
-// }
 
-const cells = document.querySelectorAll('.cell');
-const colorPicker = document.querySelector('#colorpicker');
-const reset = document.querySelector('#reset');
-const eraser = document.querySelector('#eraser');
-
-cells.forEach(cell => cell.addEventListener('mouseenter', () => cell.style.background = colorPicker.value));
-cells.forEach(cell => cell.addEventListener('click', () => cell.style.background = ''));
-reset.addEventListener('click', () => cells.forEach(cell => cell.style.background = ''));
-eraser.addEventListener('click', () => {
-    for(let i  = 0; i < cells.length; i++) {
-        cells[i].addEventListener('h-over', () => cells[i].style.background = '');
+rgb.addEventListener('click', function() {
+    let cell = gridContainer.children;
+    for (let i = 0; i < cell.length; i++) {
+        cell[i].addEventListener('mouseover', function(e) {
+            e.target.style.background = getRandomColor();
+        })
     }
 });
+
+// Reset button to clear the screen
+reset.addEventListener('click', () => cells.forEach(cell => cell.style.background = ''));
+
+colorPicker.addEventListener('input', chosenColor);
+
+eraser.addEventListener('click', eraseBtn);
